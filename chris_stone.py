72e5906b23c540b5ea20318d849472086dcf0dfb
Chris Stone <ircstones@gmail.com>
2021-05-12 14:39:48 -0700

    Python Script as a Data dictionary, changed conversion to use with, Ver2

--- chris_stone.py
+++ chris_stone.py
@@ -12,8 +12,14 @@ user_data = {
     'github_username': 'cmikestone'
 }
 
+#'''Convert the dictionary to a YAML file'''
+
+#yaml_file = open("/Users/stonech/DevNet/user_data.yaml", "w")
+#print (yaml.dump(user_data), file=yaml_file) 
+#yaml_file.close()
+
+
 '''Convert the dictionary to a YAML file'''
 
-yaml_file = open("/Users/stonech/DevNet/user_data.yaml", "w")
-print (yaml.dump(user_data), file=yaml_file) 
-yaml_file.close()
+with open(r'/Users/stonech/DevNet/user_data.yaml', 'w') as file:
+    converted = yaml.dump(user_data, file)

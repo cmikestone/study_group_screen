580f981b2b3fc1a8e260b1feaf8b0715377fd06e
Chris Stone <ircstones@gmail.com>
2021-05-12 17:20:02 -0700

    Python Script as a Data dictionary, Make one dictionary under the list, Ver4

--- chris_stone.py
+++ chris_stone.py
@@ -1,25 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import yaml
 
 '''User specific data dictionary'''
 
-user_data = dict([
-    ('first_name', 'Christopher'),
-    ('last_name', 'Stone'),
-    ('company', 'WWT'),
-    ('email', 'christopher.stone@wwt.com'),
-    ('github_username', 'cmikestone')
-])
-
-#'''Convert the dictionary to a YAML file'''
-
-#yaml_file = open("/Users/stonech/DevNet/user_data.yaml", "w")
-#print (yaml.dump(user_data), file=yaml_file) 
-#yaml_file.close()
-
+user_data = [
+    {'first_name': 'Christopher',
+    'last_name': 'Stone',
+    'company': 'WWT',
+    'email': 'christopher.stone@wwt.com',
+    'github_username': 'cmikestone'}
+]
 
 '''Convert the dictionary to a YAML file'''
 
-with open(r'/Users/stonech/DevNet/user_data.yaml', 'w') as file:
+with open('/Users/stonech/DevNet/user_data.yaml', 'w') as file:
     converted = yaml.dump(user_data, file)

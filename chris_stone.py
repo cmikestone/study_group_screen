bb790fc9a56caa6a249680faa5f0d3742366913a
cmikestone <Christopher.stone@wwt.com>
2021-05-14 08:56:13 -0700

    Added two functions: 1 for the list and 1 for the YAML conversion

--- chris_stone.py
+++ chris_stone.py
@@ -2,17 +2,25 @@
 
 import yaml
 
+def dict_list():
+    user_data = [
+         {'first_name': 'Christopher',
+          'last_name': 'Stone',
+          'company': 'WWT',
+          'email': 'christopher.stone@wwt.com',
+          'github_username': 'cmikestone'}
+]
+    return(user_data)
+    
 '''User specific data dictionary'''
 
-user_data = [
-    {'first_name': 'Christopher',
-    'last_name': 'Stone',
-    'company': 'WWT',
-    'email': 'christopher.stone@wwt.com',
-    'github_username': 'cmikestone'}
-]
+def main():
+    list_data = dict_list()
+
+    with open('/Users/stonech/DevNet/user_data.yaml', 'w') as file:
+        converted = yaml.dump(list_data, file)
 
 '''Convert the dictionary to a YAML file'''
 
-with open('/Users/stonech/DevNet/user_data.yaml', 'w') as file:
-    converted = yaml.dump(user_data, file)
+if __name__ == '__main__':
+    main()
